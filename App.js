a4c27eb1367d5790dfc275a1b692a3cfde1b3543
alanpinheiro96 <alantnt15@gmail.com>
2024-11-13 22:10:36 -0300

    Finalização do app Calculadora

--- App.js
+++ App.js
@@ -1,5 +1,9 @@
-import { useState } from 'react';
-import { SafeAreaView, View, StyleSheet } from 'react-native';
+import { useState } from 'react'
+import {
+  SafeAreaView,
+  View,
+  StyleSheet
+} from 'react-native'
 
 import Botao from "./components/Botao"
 import Visor from "./components/Visor"
@@ -22,10 +26,10 @@ let estadoTemp = {
 
 export default function App() {
 
-  const [estadoCalculadora, setEstadoCalculadora] = useState({ ...estadoInicial })
+  const [estadoCalculadora, setEstadoCalculadora] = useState({...estadoInicial})
 
   adicionarDigito = n => {
-    if (n === '.' && estadoCalculadora.valorVisor.includes('.')) {
+    if(n === '.' && estadoCalculadora.valorVisor.includes('.')){
       return
     }
 
@@ -33,22 +37,22 @@ export default function App() {
     const valorCorrente = limparVisor ? '' : estadoCalculadora.valorVisor
     const valorVisor = valorCorrente + n
 
-    if (n !== '.') {
+    if(n !== '.') {
       const novoValor = parseFloat(valorVisor)
       const valores = estadoCalculadora.valores
       valores[estadoCalculadora.posicaoCorrente] = novoValor
     }
 
-    setEstadoCalculadora({ ...estadoCalculadora, valorVisor, limparVisor: false });
+    setEstadoCalculadora({...estadoCalculadora, valorVisor, limparVisor: false});
   }
 
   limparMemoria = () => {
-    setEstadoCalculadora({ ...estadoInicial })
+    setEstadoCalculadora({...estadoInicial})
   }
 
   setOperacao = operacao => {
-    if (estadoCalculadora.posicaoCorrente === 0) {
-      setEstadoCalculadora({ ...estadoCalculadora, operacao, posicaoCorrente: 1, limparVisor: true })
+    if(estadoCalculadora.posicaoCorrente === 0) {
+      setEstadoCalculadora({...estadoCalculadora, operacao, posicaoCorrente: 1, limparVisor: true})
     } else {
       const ehIgual = operacao === '='
       const valores = [...estadoCalculadora.valores]
@@ -72,25 +76,25 @@ export default function App() {
 
   return (
     <SafeAreaView style={estilos.container}>
-      <Visor valor={estadoCalculadora.valorVisor} />
+      <Visor valor={estadoCalculadora.valorVisor}/>
       <View style={estilos.botoes}>
         <Botao label="AC" triplo onClick={limparMemoria} />
-        <Botao label="/" operacao onClick={setOperacao} />
+        <Botao label="/"  operacao onClick={setOperacao} />
         <Botao label="7" onClick={adicionarDigito} />
-        <Botao label="8" onClick={adicionarDigito} />
-        <Botao label="9" onClick={adicionarDigito} />
-        <Botao label="*" operacao onClick={setOperacao} />
-        <Botao label="4" onClick={adicionarDigito} />
-        <Botao label="5" onClick={adicionarDigito} />
-        <Botao label="6" onClick={adicionarDigito} />
-        <Botao label="-" operacao onClick={setOperacao} />
-        <Botao label="1" onClick={adicionarDigito} />
-        <Botao label="2" onClick={adicionarDigito} />
-        <Botao label="3" onClick={adicionarDigito} />
-        <Botao label="+" operacao onClick={setOperacao} />
-        <Botao label="0" duplo onClick={adicionarDigito} />
-        <Botao label="." onClick={adicionarDigito} />
-        <Botao label="=" operacao onClick={setOperacao} />
+        <Botao label="8" onClick={adicionarDigito}/>
+        <Botao label="9" onClick={adicionarDigito}/>
+        <Botao label="*" operacao onClick={setOperacao}/>
+        <Botao label="4" onClick={adicionarDigito}/>
+        <Botao label="5" onClick={adicionarDigito}/>
+        <Botao label="6" onClick={adicionarDigito}/>
+        <Botao label="-" operacao onClick={setOperacao}/>
+        <Botao label="1" onClick={adicionarDigito}/>
+        <Botao label="2" onClick={adicionarDigito}/>
+        <Botao label="3" onClick={adicionarDigito}/>
+        <Botao label="+" operacao onClick={setOperacao}/>
+        <Botao label="0" duplo onClick={adicionarDigito}/>
+        <Botao label="." onClick={adicionarDigito}/>
+        <Botao label="=" operacao onClick={setOperacao}/>
       </View>
     </SafeAreaView>
   );
